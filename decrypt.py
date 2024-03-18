14e483719b4a0f4471df15373401d4fdb0328fde
Vigneshunusual <133629748+Vigneshunusual@users.noreply.github.com>
2024-03-18 11:58:16 +0530

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,14 +1,14 @@
 from cryptography.fernet import Fernet
 
 # File paths
-keylog_file_path = r'D:\key\stroker\keylog_encrypted.txt'
-sys_file_path = r'D:\key\stroker\systeminfo_encrypted.txt'
-clipboard_file_path = r'D:\key\stroker\clipboard_encrypted.txt'
-screenshot_file_path = r'D:\key\stroker\screenshot_encrypted.png'
-audio_file_path = r'D:\key\stroker\audio_encrypted.wav'
+keylog_file_path = 'FILE PATH:where you want to save'
+sys_file_path = 'FILE PATH:where you want to save'
+clipboard_file_path = 'FILE PATH:where you want to save''
+screenshot_file_path = 'FILE PATH:where you want to save'
+audio_file_path ='FILE PATH:where you want to save'
 
 # Load the encryption key
-key = b'T3Xt6h9CTuNhCC2WSHy1UASL7KG2oO-8xlaoCDwi9U0='  # Replace with the actual encryption key
+key = ' '  # Replace with the actual encryption key
 
 # Create a cipher suite using the key
 cipher_suite = Fernet(key)
